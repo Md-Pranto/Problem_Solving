38fe752d5b0ae51e275bb6d31a23059d5957ac34
Md-Pranto <161560361+Md-Pranto@users.noreply.github.com>
2024-11-05 09:32:49 +0600

    Add files via upload

---
+++ Week 1/Random Problems/D - Four Digits.cpp
@@ -0,0 +1,48 @@
+#include<bitset>
+#include<set>
+#include<stdio.h>
+#include<string.h>
+#include<stack>
+#include<queue>
+#include<cstring>
+#include<vector>
+#include<map>
+#include<deque>
+#include<algorithm>
+#include<cmath>
+#include<cstdio>
+#include<iomanip>
+#include <iostream>
+#include<utility>
+#include<climits>
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ul;
+#define endl '\n'
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+//Optimize is not allowed when I have to take input from input.txt file
+
+int main(void){
+    optimize();
+    int a; cin>>a;
+    int digit = log10(a)+1;
+    int lagbe = 4-digit;
+    if(a==0) {
+        for(int i=0;i<3;i++) cout<<"0";
+    }
+    for(int i=0;i<lagbe;i++){
+        cout<<"0";
+    }
+    cout<<a<<endl;
+    
+    
+    
+    return 0;
+}
+
+
