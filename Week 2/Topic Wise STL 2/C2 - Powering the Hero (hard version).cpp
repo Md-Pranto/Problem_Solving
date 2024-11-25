d647c44ab5f09ee7e5badb80130a383daca16ccb
Md-Pranto <161560361+Md-Pranto@users.noreply.github.com>
2024-11-25 11:34:28 +0600

    Added new stl solution

---
+++ Week 2/Topic Wise STL 2/C2 - Powering the Hero (hard version).cpp
@@ -0,0 +1,51 @@
+#include<bitset>
+#include<set>
+#include<stdio.h>
+#include<string.h>
+#include<stack>
+#include<queue>
+#include<cstring>
+#include<vector>
+#include<map>
+#include<deque>
+#include<algorithm>
+#include<cmath>
+#include<cstdio>
+#include<iomanip>
+#include <iostream>
+#include<utility>
+#include<climits>
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ul;
+#define endl '\n'
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+//Optimize is not allowed when I have to take input from input.txt file
+void solve(){
+    int n;cin>>n;
+    priority_queue<int> pq;
+    ll ans=0;
+    for(int i=0;i<n;i++){
+        int a; cin>>a;
+        if(a) pq.push(a);
+        else {
+            if(!pq.empty()){
+                ll top = pq.top(); pq.pop();
+                ans+=top;
+            }
+        }
+    }
+    cout<<ans<<endl;
+    
+}
+int main() {
+    int tc=1; cin>>tc;
+    while(tc--) solve();
+
+}
+
