72ccffff1e49319dbe61cbf1659d07001b15bae4
Md-Pranto <161560361+Md-Pranto@users.noreply.github.com>
2024-11-26 10:27:44 +0600

    Add files via upload

---
+++ Week 2/Week 2- Random Problems Vjudge/A - Remove It.cpp
@@ -0,0 +1,48 @@
+#include<bitset>
+#include<set>
+#include<stdio.h>
+#include<string.h>
+#include<stack>
+#include<queue>
+#include<cstring>
+#include<vector>
+#include<map>
+#include<deque>
+#include<algorithm>
+#include<cmath>
+#include<cstdio>
+#include<iomanip>
+#include <iostream>
+#include<utility>
+#include<climits>
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ul;
+#define endl '\n'
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+//Optimize is not allowed when I have to take input from input.txt file
+
+void solve(){
+    int n,bad; cin>>n>>bad;
+    for(int i=0;i<n;i++){
+        int a; cin>>a;
+        if(a==bad) continue;
+        cout<<a<<" ";
+    }
+}
+int main(void){
+    optimize();
+    int tc=1; //cin>>tc;
+    while(tc--) solve();
+    
+    cout<<endl;
+    
+    return 0;
+}
+
+
