e86e640d61e2941d11154083bef659d8958e17fb
Md-Pranto <161560361+Md-Pranto@users.noreply.github.com>
2024-11-26 12:33:35 +0600

    Add files via upload

---
+++ Week 2/Practice Contest 2 - Div. 4/B - Pending Assignments CodeChef - ASSIGNMNT.cpp
@@ -0,0 +1,48 @@
+#include<bitset>
+#include<set>
+#include<stdio.h>
+#include<string.h>
+#include<stack>
+#include<queue>
+#include<cstring>
+#include<vector>
+#include<map>
+#include<deque>
+#include<algorithm>
+#include<cmath>
+#include<cstdio>
+#include<iomanip>
+#include <iostream>
+#include<utility>
+#include<climits>
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ul;
+#define endl '\n'
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+//Optimize is not allowed when I have to take input from input.txt file
+void solve(){
+    int n,t,total; cin>>n>>t>>total;
+    total*=1440;
+    int need = n*t;
+    if(total>=need) cout<<"YES";
+    else cout<<"NO";
+    cout<<endl;
+    
+}
+int main(void){
+    optimize();
+    int tc; tc = 1;
+    cin>>tc;
+    while(tc--) solve();
+    
+    
+    return 0;
+}
+
+
