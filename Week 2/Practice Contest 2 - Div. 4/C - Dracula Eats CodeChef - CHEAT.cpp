1a9674f387eb1ae194998fdea579d0acd46b3d2b
Md-Pranto <161560361+Md-Pranto@users.noreply.github.com>
2024-11-26 12:34:23 +0600

    C - Dracula Eats CodeChef - CHEAT

---
+++ Week 2/Practice Contest 2 - Div. 4/C - Dracula Eats CodeChef - CHEAT.cpp
@@ -0,0 +1,48 @@
+#include<bitset>
+#include<set>
+#include<stdio.h>
+#include<string.h>
+#include<stack>
+#include<queue>
+#include<cstring>
+#include<vector>
+#include<map>
+#include<deque>
+#include<algorithm>
+#include<cmath>
+#include<cstdio>
+#include<iomanip>
+#include <iostream>
+#include<utility>
+#include<climits>
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ul;
+#define endl '\n'
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+//Optimize is not allowed when I have to take input from input.txt file
+void solve(){
+    int a; cin>>a;
+    int cnt=0;
+    if(a>=2){
+        a-=2; cnt++;
+    }
+    cnt+= a/7;
+    cout<<cnt<<endl;
+}
+int main(void){
+    optimize();
+    int tc; tc = 1;
+    cin>>tc;
+    while(tc--) solve();
+    
+    
+    return 0;
+}
+
+
