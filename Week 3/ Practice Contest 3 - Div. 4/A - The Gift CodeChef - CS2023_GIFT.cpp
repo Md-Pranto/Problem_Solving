f8384b911b657dcfdd1e15baaac253ebed28ec2a
Md-Pranto <161560361+Md-Pranto@users.noreply.github.com>
2024-11-28 20:06:17 +0600

    Add files via upload

---
+++ Week 3/ Practice Contest 3 - Div. 4/A - The Gift CodeChef - CS2023_GIFT.cpp
@@ -0,0 +1,41 @@
+#include<bitset>
+#include<set>
+#include<stdio.h>
+#include<string.h>
+#include<stack>
+#include<queue>
+#include<cstring>
+#include<vector>
+#include<map>
+#include<deque>
+#include<algorithm>
+#include<cmath>
+#include<cstdio>
+#include<iomanip>
+#include <iostream>
+#include<utility>
+#include<climits>
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ul;
+#define endl '\n'
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+//Optimize is not allowed when I have to take input from input.txt file
+
+int main(void){
+    optimize();
+    int a,b,c; cin>>a>>b>>c;
+    if((a+c)>=b) cout<<"YES";
+    else cout<<"NO";
+    
+    cout<<endl;
+    
+    return 0;
+}
+
+
