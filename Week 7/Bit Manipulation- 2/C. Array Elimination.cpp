bab5dc509064c142e116ce308138e8167c9d233e
Md-Pranto <161560361+Md-Pranto@users.noreply.github.com>
2024-12-23 21:11:34 +0600

    Add files via upload

---
+++ Week 7/Bit Manipulation- 2/C. Array Elimination.cpp
@@ -0,0 +1,61 @@
+#include<bitset>
+#include<set>
+#include<stdio.h>
+#include<string.h>
+#include<stack>
+#include<queue>
+#include<cstring>
+#include<vector>
+#include<map>
+#include<deque>
+#include<algorithm>
+#include<cmath>
+#include<cstdio>
+#include<iomanip>
+#include <iostream>
+#include<utility>
+#include<climits>
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ul;
+#define int long long
+#define endl '\n'
+#define all(x) x.begin(),x.end()
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+//Optimize is not allowed when I have to take input from input.txt file
+
+void solve(){
+    int n; cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++) cin>>v[i];
+    vector<int> cnt(33,0);
+    for(int i=0;i<32;i++){
+        
+        for(int j=0;j<n;j++){
+            if((v[j]>>i)&1) cnt[i]++;
+        }
+    }
+    for(int k=1;k<=n;k++){
+        bool flag = true;
+        for(int j=0;j<32;j++){
+            if(cnt[j]%k!=0){
+                flag = false; break;
+            }
+        }
+        if(flag) cout<<k<<" ";
+    }
+    cout<<endl;
+}
+int32_t main(void){
+    optimize();
+    int tc = 1; cin>>tc;
+    while(tc--) solve();
+    
+    
+    return 0;
+}
+ 
