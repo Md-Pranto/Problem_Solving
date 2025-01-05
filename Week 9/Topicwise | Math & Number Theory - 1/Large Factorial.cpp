169616707a474c56a9e38eef14c668d09d2655d8
Md-Pranto <161560361+Md-Pranto@users.noreply.github.com>
2025-01-05 22:32:52 +0600

    Add files via upload

---
+++ Week 9/Topicwise | Math & Number Theory - 1/Large Factorial.cpp
@@ -0,0 +1,54 @@
+#include<bitset>
+#include<set>
+#include<stdio.h>
+#include<string.h>
+#include<stack>
+#include<queue>
+#include<cstring>
+#include<vector>
+#include<map>
+#include<deque>
+#include<algorithm>
+#include<cmath>
+#include<cstdio>
+#include<iomanip>
+#include <iostream>
+#include<utility>
+#include<climits>
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ul;
+#define int long long
+#define endl '\n'
+#define all(x) x.begin(),x.end()
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+//Optimize is not allowed when I have to take input from input.txt file
+int mod = 1LL*1000000007;
+int fact(int n){
+    int ans=1LL;
+    while(n>0){
+        ans*=n*1LL;
+        ans= (ans%mod);
+        n--;
+    }
+    return ans;
+}
+void solve(){
+    int n; cin>>n;
+    cout<<fact(n)<<endl;
+    
+    
+}
+int32_t main(void){
+    optimize();
+    int tc = 1; cin>>tc;
+    while(tc--) solve();
+    
+    
+    return 0;
+}
+ 
